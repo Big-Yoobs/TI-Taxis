bf9927a4e7494653116e349b6ea240bdd15d7bc7
Thomas Richards <106329898+tomphonse@users.noreply.github.com>
2022-11-07 14:05:28 +1300

    Included Nlohmann JSON lib

--- Assignment 103.2.cpp
+++ Assignment 103.2.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "libs/nlohmann/json.hpp"
 #include "commonFunctions.h"
 #include "Menu.h"
+
+using json = nlohmann::json;
 
 int main() {
     //creating class instances
